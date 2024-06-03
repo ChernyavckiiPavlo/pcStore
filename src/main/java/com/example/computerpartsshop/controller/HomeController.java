af2765c407b9a0f944748e8ae56ab2144524b295
Pavel_Chernyavckii <Chernyavckii15@gmail.com>
2024-06-03 21:37:02 +0300

    почти все работает, кроме подробного результата после поиска

--- src/main/java/com/example/computerpartsshop/controller/HomeController.java
+++ src/main/java/com/example/computerpartsshop/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -17,6 +18,13 @@ public class HomeController {
     @Autowired
     public HomeController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @GetMapping("/products/details/{id}")
+    public String viewProductDetails(@PathVariable("id") Long id, Model model) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "product-details";
     }
 
     @GetMapping("/home")
